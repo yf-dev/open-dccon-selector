aea8a1a0a40daed294ba4ccdd3c77c6a669ee0f1
Nesswit <rishubil@gmail.com>
2017-09-25 23:17:04 +0900

    Add build and zip script

---
+++ build-frontend.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [ "$(id -u)" != "0" ]; then
+	echo "Sorry, you are not root."
+	exit 1
+fi
+
+rm -r ./frontend/dist/*
+docker-compose -f docker-compose.build-frontend.yml up
+cd ./frontend/dist
+zip -r ods-$(date +'%Y%m%d-%H%M%S').zip ./*
+
