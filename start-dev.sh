39256a64eb9842d53ca84511fe04358feab6238c
Nesswit <rishubil@gmail.com>
2018-01-12 02:09:04 +0900

    Now start-dev script stop flask only

--- start-dev.sh
+++ start-dev.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker-compose -f docker-compose.dev.yml stop && docker-compose -f docker-compose.dev.yml up -d && docker-compose -f docker-compose.dev.yml logs -f -t --tail=100
+docker-compose -f docker-compose.dev.yml stop flask && docker-compose -f docker-compose.dev.yml up -d && docker-compose -f docker-compose.dev.yml logs -f -t --tail=100
