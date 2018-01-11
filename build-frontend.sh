c11bab50e38a3f634b398a1a14f06f4cdfdc54a2
Nesswit <rishubil@gmail.com>
2018-01-12 03:09:30 +0900

    Remove root permission check and zip on build-frontend script

--- build-frontend.sh
+++ build-frontend.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-if [ "$(id -u)" != "0" ]; then
-	echo "Sorry, you are not root."
-	exit 1
-fi
-
 rm -r ./frontend/dist/*
 docker-compose -f docker-compose.build-frontend.yml run --rm build
-cd ./frontend/dist
-zip -r ods-$(date +'%Y%m%d-%H%M%S').zip ./*
 
