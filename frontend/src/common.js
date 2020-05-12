2d3e0244b0ece5d58a355692a18676c11df3aa07
Nesswit <rishubil@gmail.com>
2020-05-12 22:13:18 +0900

    Fix issue about font loading

--- frontend/src/common.js
+++ frontend/src/common.js
@@ -7,6 +7,6 @@ Vue.config.productionTip = false;
 WebFont.load({
   custom: {
     families: ['Spoqa Han Sans'],
-    urls: ['/static/spoqa-han-sans/SpoqaHanSans-kr.css'],
+    urls: ['static/spoqa-han-sans/SpoqaHanSans-kr.css'],
   },
 });
